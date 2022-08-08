4d839134bd3cc6d1ed52a13481035a226fd72592
user11 <e@im.lostwithoutya>
2022-08-08 20:25:25 +0700

    minor refactoring

--- src/code/useAudioNodesBasicConnectToDest.tsx
+++ src/code/useAudioNodesBasicConnectToDest.tsx
@@ -24,40 +24,87 @@ import { AudioSourceNode, AudioSinkNode } from "./useAudioNodesBasicS";
    
 
 
+      
 
-
+const M_ONMOUNT_MKCONNECT_ONUNMOUNT_DISCONNECT = 1 ;       
+/**  
+ * CAVEAT.  
+ * due to the presence of `disconnect(void )` on *mount-time*, 
+ * this mode will only work properly in *single-dest* setyting.  
+ */  
+const M_ONMOUNT_RECONNECT_ONUNMOUNT_DISCONNECT = 1.5 ;         
+/**            
+ * CAVEAT.   
+ * at this point, there's no guarantee of subsequent the `useYyyEffect` call(back).  
+ * @deprecated                          
+ */
+const M_ONMOUNT_RECONNECT_ONUNMOUNT_NOOP = 2 ;
 const useAudioNodeConnectToDest = (
-    (src: AudioSourceNode | null, destination: (AudioSinkNode ) | null, { cncDebug = false } : { cncDebug ?: boolean ; } = {} ): void => {                   
-        ;                                 
+    (...[      
+        src , destination , 
+        { cncDebug = false, omitUnmountTimeDisconnectiveCall: omitDisconnectiveCallOnUnmount = false } = {} ,  
+    ] : [         
+        src: AudioSourceNode | null,            
+        destination: (AudioSinkNode ) | null, 
+        properties ?: {    
+            cncDebug ?: boolean ;           
+            omitUnmountTimeDisconnectiveCall ?: boolean ;     
+        }  ,     
+    ]): void => {        
+        const mode1 = (
+            (      
+                omitDisconnectiveCallOnUnmount ? 
+                M_ONMOUNT_RECONNECT_ONUNMOUNT_NOOP      
+                : M_ONMOUNT_MKCONNECT_ONUNMOUNT_DISCONNECT               
+            )  as (
+                never              
+                | typeof M_ONMOUNT_MKCONNECT_ONUNMOUNT_DISCONNECT 
+                | typeof M_ONMOUNT_RECONNECT_ONUNMOUNT_DISCONNECT      
+                | typeof M_ONMOUNT_RECONNECT_ONUNMOUNT_NOOP       
+            )              
+        ) ;                  
+        ;                   
         useLayoutEffect(() => {                 
             cncDebug && (console.log({ src, destination }), console.log(new DbgException(`ConnectToDest - printed some debug at cll ctx `) ) ) ;                
             if (src && destination) {  
-                switch (1  as number ) {             
-                    case 1 : {                                                                                            
-                        ;                             
-                        src.connect(destination) ;                 
-                        return () => {     
-                            // for some reason, if 'dest' were null, 
-                            destination && src.disconnect(destination ) ; 
+                switch (mode1 ) {      
+                    case M_ONMOUNT_MKCONNECT_ONUNMOUNT_DISCONNECT : {                                                                                                   
+                        ;          
+                        src.connect(destination) ;                       
+                        return () => {           
+                            src.disconnect(destination ) ;          
                         } ;                     
+                        break ;                    
+                    }                       
+                    case M_ONMOUNT_RECONNECT_ONUNMOUNT_DISCONNECT : {           
+                        src.disconnect() ; 
+                        src.connect(destination ) ;                                    
+                        return () => {          
+                            /**              
+                             */
+                            src.disconnect( destination ) ; 
+                            ;        
+                        } ;  
+                        break ;  
                     }
-                    case 2 : {         
+                    case M_ONMOUNT_RECONNECT_ONUNMOUNT_NOOP : {         
                         src.disconnect() ; 
                         src.connect(destination ) ;                                    
                         return () => {
-                            ;
-                        } ;
+                            ;      
+                        } ;  
+                        break ;  
                     }
                 }
             }                        
-        }, [src, destination ] );               
+        }, [src, destination, mode1 ] );               
     }      
 ) ;      
 
 
 
 
-
+    
 
 
 
