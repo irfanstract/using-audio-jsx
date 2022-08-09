bcb0c6035103e91f0bbcea416e78288ae5578763
user11 <e@im.lostwithoutya>
2022-08-09 11:20:20 +0700

    code minor refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@
 
 import React from 'react';           
 import { K } from './code/commonElements';   
-import componentWithLoadingScreen from './code/componentWithLoadingScreen';       
+import componentWithLoadingScreen from './code/componentWithLoadingScreen';  
+import { AppWrapper1 } from './code/useAppWrapper1';       
 // asset imports
-import logo from './logo.svg';
-// App Code Imports
+import logo from './logo.svg';   
+// App Code Imports   
 import { AudioLoopDemoApp } from './code/audioLoopDemo';  
    
 // CSS imports         
-import './App.css';
+import './App.css';   
 
 function OriginalApp() {
   return (
@@ -52,43 +53,7 @@ function LoadingScreenApp() {
     );   
   }   
           
-/**   
- * important consideration :  
- * - needs Error-bounding ; 
- *   otherwise HMR will not restart the mounting even on save .  
- * - needs {@link React.Suspense }       
- * - see "process is not defined, on hot-reload"
- */         
-const AppWrapper1 = (App: () => React.ReactElement ) => {    
-  const key = (    
-    String(Math.random() )         
-  ) ;                             
-  console.log({ k: key }) ;
-  class App1 extends React.Component<{}, { error ?: unknown } > {   
-    render() {                  
-      return (  
-        <K key={key }> 
-        <p> Application (id: <code>{key }</code> ) </p>
-        <React.Suspense fallback={ <div /> } >
-          { (this.state || {} ).error ? null : <App />    }       
-        </React.Suspense>            
-        </K>                    
-      ) ;                 
-    }         
-    componentDidCatch(e: unknown ) { 
-      console.error(e) ; 
-      this.setState({ error: e }) ;
-      setTimeout(() => {                  
-        (window.location).reload() ;           
-      } , 1 * 60 * 1000 ) ;       
-    }   
-  }        
-  return (): React.ReactElement => (
-    <K key={key} >   
-      <App1/>   
-    </K>                 
-  ) ;           
-} ;        
+;    
 export default (    
   function AppApp() {                  
     return ( 
