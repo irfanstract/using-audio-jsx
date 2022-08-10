490f0afce450fc6c57b335002737555f9e5aa76b
user11 <e@im.lostwithoutya>
2022-08-10 15:01:56 +0700

    clarification

--- src/code/useAudioNodesParamChgEffect1.ts
+++ src/code/useAudioNodesParamChgEffect1.ts
@@ -1,31 +1,35 @@
 
 import React, { 
-    useState, useReducer, useLayoutEffect, useEffect, useCallback, useMemo, useContext, useDeferredValue, useRef ,
+    useState, useReducer, 
+    useInsertionEffect,  useLayoutEffect, useEffect, 
+    useCallback, useMemo, useContext, useDeferredValue, 
+    useRef ,
+    useImperativeHandle ,   
 } from "react";
+     
+    
+                
 
 
 
 
 
-
-
-
-
+      
 /**     
- * which of the two ({@link useEffect } and {@link useLayoutEffect} ) 
- * .
+ * which of the two ({@link useEffect } and {@link useLayoutEffect} and {@link useInsertionEffect } ) 
+ * - {@link useEffect } - timing is equivalent to {@link useDeferredValue }  
+ * - {@link useLayoutEffect } - timing is equivalent to `componentDidUpdate`   
+ * - {@link useInsertionEffect } - prior to (display-level) comission
  *           
- * currently that should be `"useLayoutEffect"`, but .                
- * normally,    
- * React-based apps should choose `useEffect` since      
+ * currently that should be `"useLayoutEffect"`.                                     
+ * normally,            
+ * React-based apps should choose {@link useEffect `useEffect` } since      
  * use of `uselayoutEffect` can easily overhead causing the platform to become unresponsive and
  * `useEffect` overcomes this by deferring the dispatch(es) .      
  * however, when it comes to {@link AudioNode}s, 
- * the patterns of timing distortions introduced by {@link useEffect} can intensely disrupt the synth, so       
- * the codebase had to switch to `useLayoutEffect` .                   
- * note that React yet has another one, `useInsertionEffect`, but 
- * support is quite limited, and that's probably unnecessary given `useLayoutEffect`
-*/     
+ * the patterns of timing distortions introduced by {@link useEffect} (and, to less extent, {@link useLayoutEffect} ) can intensely disrupt the synth, so       
+ * the codebase had to switch to `useLayoutEffect` .                    
+*/          
 const AUDIONODES_USEEFFECT : (
     keyof Pick<typeof React, "useEffect" | "useLayoutEffect" | "useInsertionEffect" >
 ) = (
