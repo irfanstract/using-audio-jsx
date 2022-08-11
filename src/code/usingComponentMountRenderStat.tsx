13f880b112bc458b2a340e738a7765711e73dd68
meh <eh@ah>
2022-08-11 14:32:41 +0700

    docs

--- src/code/usingComponentMountRenderStat.tsx
+++ src/code/usingComponentMountRenderStat.tsx
@@ -12,23 +12,35 @@ import React, { useMemo } from "react";
 
   
 
-const useRenderCount = (
+/**        
+ * this integer will increment every `render()`.    
+ * 
+ * this has use in debugging whether a Component suffers from *brute-force re-renders*.   
+ */         
+const useRenderCount = (  
     () => (
         React.useRef<number>(0 ).current  ++          
-    )      
+    )          
 ) ;
-const useDepsChgCount = (
-    function (...[{} , deps ] : [{} , React.DependencyList ]) {
+/**    
+ * this integer will increment {@link React.useEffect everytime `deps` changes }.  
+ * this facility can be utilised to probe the in-so-far number of changes of `deps`.  
+ * 
+ * this has use in       
+ * debugging whether a *{@link React.useState state}, {@link React.useMemo }* suffers from *brute-force re-renders*.   
+ */     
+const useDepsChgCount = (        
+    function (...[{} , deps ] : [constraints: {} , deps: React.DependencyList ]) {
         const r = (
             React.useRef<number>(0 )   
-        ) ;            
+        ) ;               
         return (
             // TODO
             React.useMemo(() => (r.current ++ ) , deps )
         ) ;
     }   
 ) ;     
-
+  
 function useDebugDispatcher <A extends void | (null | true | object ) > (...[] : [
    //  
 ]): React.Dispatch<() => A > {        
