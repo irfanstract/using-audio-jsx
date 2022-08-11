9057d952613361209b2df758fc1f468959190651
meh <eh@ah>
2022-08-11 10:29:45 +0700

    urgent clarification, and internal refactoring

--- src/code/useAudioNodesParamChgEffect1.ts
+++ src/code/useAudioNodesParamChgEffect1.ts
@@ -18,24 +18,47 @@ import React, {
 /**     
  * which of the two ({@link useEffect } and {@link useLayoutEffect} and {@link useInsertionEffect } ) 
  * - {@link useEffect } - timing is equivalent to {@link useDeferredValue }  
- * - {@link useLayoutEffect } - timing is equivalent to `componentDidUpdate`   
- * - {@link useInsertionEffect } - prior to (display-level) comission
- *           
- * currently that should be `"useLayoutEffect"`.                                     
+ * - {@link useLayoutEffect } - timing is equivalent to `componentDidUpdate`  
+ *            
+ * {@link AUDIONODES_USEEFFECT } is for arbitrary usage, while
+ * {@link AUDIONODES_USE_AUDIONODEEFFECT} is meant only for 
+ * {@link AudioNode}s setup which do not {@link React.useState invoke `dispatch`-or-`setState` }.  
+ * 
+ * currently that should be `"useLayoutEffect"`.                               
  * normally,            
  * React-based apps should choose {@link useEffect `useEffect` } since      
  * use of `uselayoutEffect` can easily overhead causing the platform to become unresponsive and
  * `useEffect` overcomes this by deferring the dispatch(es) .      
  * however, when it comes to {@link AudioNode}s, 
  * the patterns of timing distortions introduced by {@link useEffect} (and, to less extent, {@link useLayoutEffect} ) can intensely disrupt the synth, so       
- * the codebase had to switch to `useLayoutEffect` .                    
+ * the codebase had to switch to `useLayoutEffect` .         
+ *                                
 */          
 const AUDIONODES_USEEFFECT : (
     keyof Pick<typeof React, "useEffect" | "useLayoutEffect" | "useInsertionEffect" >
 ) = (
     "useLayoutEffect"     
 ) ;      
-const AUDIONODES_USEEFFECT1 : (
+/**     
+ * which of the two ({@link useEffect } and {@link useLayoutEffect} and {@link useInsertionEffect } ) 
+ * - {@link useEffect } - timing is equivalent to {@link useDeferredValue }  
+ * - {@link useLayoutEffect } - timing is equivalent to `componentDidUpdate`   
+ * - {@link useInsertionEffect } - prior to (display-level) comission
+ *                
+ * {@link AUDIONODES_USEEFFECT } is for arbitrary usage, while
+ * {@link AUDIONODES_USE_AUDIONODEEFFECT} is meant only for 
+ * {@link AudioNode}s setup which do not {@link React.useState invoke `dispatch`-or-`setState` }.  
+ * 
+ * currently that should be `"useLayoutEffect"`.                               
+ * normally,            
+ * React-based apps should choose {@link useEffect `useEffect` } since      
+ * use of `uselayoutEffect` can easily overhead causing the platform to become unresponsive and
+ * `useEffect` overcomes this by deferring the dispatch(es) .      
+ * however, when it comes to {@link AudioNode}s, 
+ * the patterns of timing distortions introduced by {@link useEffect} (and, to less extent, {@link useLayoutEffect} ) can intensely disrupt the synth, so       
+ * the codebase had to switch to `useLayoutEffect` .                         
+*/                 
+const AUDIONODES_USE_AUDIONODEEFFECT : (
     keyof (
         Pick<typeof React, "useInsertionEffect" | "useLayoutEffect" >
     )
@@ -60,5 +83,5 @@ const AUDIONODES_USEEFFECT1 : (
 
 export {
     AUDIONODES_USEEFFECT , 
-    AUDIONODES_USEEFFECT1 as AUDIONODES_USE_AUDIONODEEFFECT ,        
-} ;     
+    AUDIONODES_USE_AUDIONODEEFFECT   ,        
+} ;                
