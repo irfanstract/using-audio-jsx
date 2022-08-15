8610638d2cabbc372370e1d38d31d78fcf8ad13e
meh <eh@ah>
2022-08-15 20:22:07 +0700

    change

--- src/code/audioLoopDemo.tsx
+++ src/code/audioLoopDemo.tsx
@@ -114,7 +114,7 @@ export const AudioLoopDemoApp = (function () {
                 useASetCurrentT(_1?.feedPt.context || null )    
             ) ;           
             const c1  = (  
-                <WithSlowdown value={1 } >
+                <WithSlowdown value={2 ** 0.5 } >
                 <WithNSecondsFadeInBF>
                   <CBassDrumLoop />       
                   <CAmbientNoise />
