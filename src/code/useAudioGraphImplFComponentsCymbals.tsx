a243e4311d521901fd3319d8733ff5b4125b15db
meh <eh@ah>
2022-08-17 22:59:26 +0700

    initial

---
+++ src/code/useAudioGraphImplFComponentsCymbals.tsx
@@ -0,0 +1,43 @@
+
+
+
+
+
+          
+import { 
+   CFnValue1 ,
+   
+   CHalfSecndBeep1 , CPersistingBeep , CWaveTable1 , CWhiteNoise ,    
+   CBassDrumKickFluidly1 ,
+   CSnareDrumFluidly1 , 
+   
+   CAmpModulated , CBiquadFilterModulated ,  CFreqDmAnalyF ,  CConstantValue , CAmpModulated0 ,    
+
+   MetronomeCheckAndExpandingElem , 
+   
+} from "./audioLoopDemoComponents1"; 
+
+
+
+
+
+
+
+
+
+
+
+
+const CContinousHiHatFluidly = (
+   () => (
+      <CAmpModulated0 value={<CConstantValue value={2 ** -2 } /> } >
+            <CSnareDrumFluidly1 />
+      </CAmpModulated0> 
+   )
+);
+
+
+
+export {
+   CContinousHiHatFluidly ,
+} ;
