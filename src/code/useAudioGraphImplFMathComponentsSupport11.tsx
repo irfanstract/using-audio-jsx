04094aa9c9930ab0d77052f6836fbfa90eabd001
meh <eh@ah>
2022-08-18 09:35:13 +0700

    redirect or drop the imports

--- src/code/useAudioGraphImplFMathComponentsSupport11.tsx
+++ src/code/useAudioGraphImplFMathComponentsSupport11.tsx
@@ -16,40 +16,15 @@ import { K } from "./commonElements";
         
 
 // domain-imports        
-import * as tCtxs from "./useAudioGraphImplAbsoluteTCtx1";   
-import { useACtxMtWithoutAnyFilter1 } from "./useAudioNodexCtxInitAndBeepNcaOnce1";
-import { 
-   CHalfSecndBeepAtAbsoluteT ,    
-   CAmpModulated as CAmpModulatedTimeDomain ,  
-   CAmpSlideDownAtAbsoluteT ,   
-   CBiquadFilterModulated ,             
-     
-   CPersistingBeep as CPersistingBeep0 ,       
-   CConstantValueModulated ,     
-   CConstantValue ,      
-   CFnValue as CFnValueAtAbsoluteT ,    
-      
-   nonterminalUsageWrapC ,   
-   terminalUsageWrapC ,          
-   modulatedWaveTableUsageWrapC ,      
- 
-} from "./useAudioGraphImplCurrentDestNdRefCtxC";           
+import * as tCtxs from "./useAudioGraphImplAbsoluteTCtx1";    
+import { CAmpModulated as CAmpModulatedTimeDomain } from "./useAudioGraphImplCurrentDestNdRefCtxC";       
+import { CPersistingBeep as CPersistingBeep0 } from "./useAudioGraphImplCurrentDestNdRefCtxC";
+import { CFnValue as CFnValueAtAbsoluteT } from "./useAudioGraphImplFMathComponentsSupport1";
 import {
    Consm as WithCurrentDestNdRef , 
    useWithCurrentSideTapPtRef , 
 
 } from "./useAudioGraphImplCurrentDestNdRefCtx";            
-import * as audioFltAtAbsTNodes from "./useAudioNodesBasicUseBeep";      
-import * as audioFrqAnlyAtAbsTNodes from "./useAudioNodesCrossmasking1";     
-import { CFreqDmAnalyF } from "./useAudioGraphImplFComponentsAnalyticalF";    
-import { 
-   LoopingWithPeriod , 
-   LoopingWithPeriodAndAutoUnmounting , 
-   LoopingWithPeriodSimple , 
-   MetronomeCheckAndExpandingElem ,  
-    
-} from "./useAudioGraphImplFComponentsLoopingWrapper1";
-import { WithAutoUnmount } from "./useAudioGraphImplFComponentsSlapCompAutoUnmounting";   
 
 // CSS
 import "./useAudioGraphImplFComponentsSty.css"     ;
