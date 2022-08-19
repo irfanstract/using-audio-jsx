ee0190de2c2205a95875aeb651452821ccaef493
meh <eh@ah>
2022-08-19 18:15:19 +0700

    initial

---
+++ src/code/useAudioGraphImplIntrinsicsInvariants1.tsx
@@ -0,0 +1,110 @@
+ 
+// utility imports                  
+import Immutable from "immutable";        
+import { IterableOps, PromiseReturnValue, OmitM } from "./generalUse11";  
+import { ABandpassFreqArgInputRangeMode } from "./audioFltBandpassMetaRangeMode";  
+import React, { useMemo } from "react";               
+import { ComponentProps, ContextReturnType } from "./commonElementsTypes";        
+import { K, asVoidElement, NUMERIC } from "./commonElements";             
+import { CBC } from "./useStateInCallback";  
+import { useRealTimeQueryInterval1 } from "./useNonHookValue";     
+import { useDeferredTrue as useDeferredTrue0 } from "./usingDeferredBoolean";       
+import { 
+    useOneWayCheckBox , 
+} from "./useCompletion";   
+          
+// 
+import {  
+    WithGivenDest ,  
+    Prv1 ,         
+
+    Consm as WithCtxtualOut ,     
+    useWithCurrentSideTapPtRef , 
+
+} from "./useAudioGraphImplCurrentDestNdRefCtx";      
+import { usePersistingBeep, YyyUsable } from "./useAudioNodesBasicFlt1";        
+import {                   
+    useHalfSecondBeep as useHalfSecondBeepAtAbsoluteT  ,   
+                  
+} from "./useAudioNodesBasicUseBeep"; 
+import * as audioFltAtAbsTNodes from "./useAudioNodesBasicFlt11";           
+import { CFreqDmAnalyF1 } from "./useAudioNodesFreqDmAnalysisC";      
+import { USEM } from "./useAudioNodesParamAutomativeAsRegularNodes";   
+import { graphAfterNrmInterpretativeMode } from "./useAudioGraphAsBandFreqCtrlMode";   
+import {
+    SpecialUsageExplainer, 
+    xWithUsableYyy ,   
+    xWithUsableYyy1 ,   
+} from "./useAudioGraphImplUsableYyyNodes1";        
+import { 
+    useConstantParamSrcNodeWithGivenFadeoutTimeConstant1 , 
+      
+} from "./useAudioNodesMounting11";  
+import {  
+    useElasUsageOnMount,
+    useGainElas ,   
+    useGainElasD ,     
+    useConstantParamSrcElas ,          
+    useConstantParamSrcElasD ,    
+
+} from "./useAudioNodesBasicFixedElas";    
+import {
+    // XWithInterpretation ,    
+   
+    FreqArgsProps , 
+    DetuningProps ,  
+    GainValArgsProps , 
+    GainValArgsProps1 ,   
+    QFactualProps ,  
+} from "./useAudioGraphImplBiquadFltPropsTyping";  
+import { 
+    automativeInputRangeDefaultMode ,  
+
+    BiquadFltCProps ,               
+    biquadFltCPropsParse , 
+    WaveTableNodeProps , 
+    waveTableCPropsShallParse ,     
+    ParamAutomativeNodeCProps , 
+    evParamAutomativeNodeCPropsParse , 
+    evSingleArgumentTerminalNodeCPropsParse , 
+     
+} from "./useAudioGraphImplBasicFltAutomableProps";     
+import {
+    terminalUsageWrapC ,    
+    nonterminalUsageWrapC , 
+ 
+    AGCtxtualOutUsageProps ,           
+    CTXTUALOUTPUTUSAGE_CBC , 
+} from "./useAudioGraphImplCurrentDestNdRefCtxC1";
+import { useAParamModulativeNode } from "./useAudioNodesParamAutomative1";  
+import { 
+    cFnValueCompImplSupport1 ,   
+
+    CConstantValue , 
+    CFnValue ,     
+              
+} from "./useAudioGraphImplFMathComponentsSupport1";    
+import {    
+    useUnexpectedZeroingBackCheck ,      
+} from "./useAudioNodesParamAutomativeElas";   
+import {   
+    useDeferredTrue ,   
+    useDeferredTrue1 as useDeferredTrueAlt ,             
+  
+    DbbOrHovered ,
+    dBBC as dBBC0 , 
+
+} from "./useAudioGraphImplUseDeferredBoolean";     
+import { XDC } from "./useAudioGraphImplFComponentsSemanticsBasic";
+import "./useAudioGraphImplCu.css";
+
+         
+ 
+  
+                     
+                          
+              
+                                
+       
+;        
+const dBBC = dBBC0 ;
