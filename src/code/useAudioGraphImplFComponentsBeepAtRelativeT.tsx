e10ab4dbea93439a6ebe9f4d7c01822e146b95c9
meh <eh@ah>
2022-08-19 14:13:46 +0700

    redirect or drop a large number of imports and work on the docs

--- src/code/useAudioGraphImplFComponentsBeepAtRelativeT.tsx
+++ src/code/useAudioGraphImplFComponentsBeepAtRelativeT.tsx
@@ -17,39 +17,13 @@ import { K } from "./commonElements";
 
 // domain-imports        
 import * as tCtxs from "./useAudioGraphImplAbsoluteTCtx1";   
-import { useACtxMtWithoutAnyFilter1 } from "./useAudioNodexCtxInitAndBeepNcaOnce1";
-import { 
-   CHalfSecndBeepAtAbsoluteT ,    
-   CAmpModulated as CAmpModulatedTimeDomain ,  
-   CAmpSlideDownAtAbsoluteT ,   
-   CBiquadFilterModulated ,             
-     
-   CPersistingBeep as CPersistingBeep0 ,       
-   CConstantValueModulated ,     
-   CConstantValue ,      
-   CFnValue as CFnValueAtAbsoluteT ,    
-      
-   nonterminalUsageWrapC ,   
-   terminalUsageWrapC ,          
-   modulatedWaveTableUsageWrapC ,      
- 
-} from "./useAudioGraphImplCurrentDestNdRefCtxC";           
+import { CHalfSecndBeepAtAbsoluteT } from "./useAudioGraphImplCurrentDestNdRefCtxC";       
 import {
    Consm as WithCurrentDestNdRef , 
    useWithCurrentSideTapPtRef , 
 
 } from "./useAudioGraphImplCurrentDestNdRefCtx";            
 import * as audioFltAtAbsTNodes from "./useAudioNodesBasicUseBeep";      
-import * as audioFrqAnlyAtAbsTNodes from "./useAudioNodesCrossmasking1";     
-import { CFreqDmAnalyF } from "./useAudioGraphImplFComponentsAnalyticalF";    
-import { 
-   LoopingWithPeriod , 
-   LoopingWithPeriodAndAutoUnmounting , 
-   LoopingWithPeriodSimple , 
-   MetronomeCheckAndExpandingElem ,  
-    
-} from "./useAudioGraphImplFComponentsLoopingWrapper1";
-import { WithAutoUnmount } from "./useAudioGraphImplFComponentsSlapCompAutoUnmounting";   
 
 // CSS
 import "./useAudioGraphImplFComponentsSty.css"     ;
@@ -73,6 +47,9 @@ const {
        tCtxs.currentTInfCtx.Consumer
    ) ;       
    return {
+      /**   
+       * half-second beep.
+       */
        CHalfSecndBeep1 : (                                
            function C1() {    
                const { t } = (
