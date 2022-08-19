4511aa3e578277bf24d22696bc3105afc6b1ac00
meh <eh@ah>
2022-08-19 16:44:31 +0700

    delete spare snippet

--- src/code/useAudioCtxCurrentTime.tsx
+++ src/code/useAudioCtxCurrentTime.tsx
@@ -10,65 +10,6 @@ import React, {
 } from "react"; 
 import { usingInterval } from "./usingTimeoutOrInterval";  
 import { useCanForceRefresh, useRealTimeQueryInterval1, useRefreshByInterval } from "./useNonHookValue";
-// const isWindowActive = (() => {           
-//     let F : boolean = false ;               
-//     window.addEventListener("pointerdown" , () => { F = true  ; } ) ;    
-//     window.addEventListener("mousedown" , () => { F = true  ; } ) ;     
-//     window.addEventListener("keydown" , () => { F = true  ; } ) ;                 
-//     window.addEventListener("focus" , () => { F = true  ; } ) ;                                 
-//     window.addEventListener("blur"  , () => { F = false ; } ) ;                           
-//     return (
-//         () => (
-//             F        
-//         )        
-//     ) ;                                                                                    
-// })() ;     
-// const useWindowActivityStatus = (
-//     () => {
-//         const [s, setS] = (
-//             useState<boolean>(false )    
-//         ) ;  
-
-//         const REFRESH = () => {        
-//             setTimeout(() => {
-//                 ;
-//                 const f = ( 
-//                     isWindowActive()                      
-//                 ) ;              
-//                 console.log({ f  }) ;    
-//                 setS(() => f ) ;         
-//             } , 250 ) ;                            
-//         };  
-
-//         useEffect(() => {
-//             REFRESH() ;  
-//             const focusAndBlurListener : {             
-//                 onFocus : () => void ; 
-//                 onBlur: () => void ; 
-//             } = {             
-//                 onFocus: REFRESH ,  
-//                 onBlur: REFRESH ,  
-//             } ;                                              
-//             {      
-//                 ;              
-//                 window.addEventListener("focus", focusAndBlurListener.onFocus ) ;          
-//                 window.addEventListener("blur" , focusAndBlurListener.onBlur  ) ;                   
-//             }  
-//             return () => {
-//                 ;                                              
-//                 {       
-//                     ;              
-//                     window.removeEventListener("focus", focusAndBlurListener.onFocus ) ;          
-//                     window.removeEventListener("blur" , focusAndBlurListener.onBlur  ) ;                   
-//                 }        
-//             } ;
-//         }, [] ) ;
-
-//         return {
-//             isWindowOnFocus : s ,
-//         } as const ;
-//     }    
-// ) ;
 import { isWindowActive, useWindowActivityStatus } from "./useWindowFocusState";        
 
 
