9a774d7a79d0a8078ea3a05a0d26306ed334eabc
meh <eh@ah>
2022-08-19 13:05:31 +0700

    delete whole files

--- src/code/useAudioSynthNodeG.tsx
+++
@@ -1,9 +0,0 @@
-
-
-
-console.error((
-    TypeError((
-        `legacy import with unsatisfactory naming. please switch to that at the new path directly` 
-    ))
-) ) ;   
-export * from "./useAudioNodesMounting11" ;
--- src/code/useAudioYyyNodeO.tsx
+++
@@ -1,7 +0,0 @@
-
-console.error((
-    TypeError((
-        `legacy import with unsatisfactory naming. please switch to that at the new path directly` 
-    ))
-) ) ;   
-export * from "./useAudioNodesMounting1" ;
