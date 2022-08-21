f95d684f96316ae0c8676194269a87d7881af642
meh <eh@ah>
2022-08-21 07:34:53 +0700

    cleanup

--- src/code/useAudioGraphImplFComponentsCymbals.tsx
+++ src/code/useAudioGraphImplFComponentsCymbals.tsx
@@ -4,21 +4,8 @@
 
 
           
-// import { 
-//    CFnValue1 ,
-   
-//    CHalfSecndBeep1 , CPersistingBeep , CWaveTable1 , CWhiteNoise ,    
-//    CBassDrumKickFluidly1 ,
-//    CSnareDrumFluidly1 , 
-   
-//    CAmpModulated , CBiquadFilterModulated ,  CFreqDmAnalyF ,  CConstantValue , CAmpModulated0 ,    
-
-//    MetronomeCheckAndExpandingElem , 
-   
-// } from "./useAudioGraphImplFComponents"; 
-// CConstantValue , CAmpModulated0 , CSnareDrumFluidly1 ,
 import { CConstantValue } from "./useAudioGraphImplFMathComponentsSupport1";
-import { CAmpModulated as CAmpModulated0 } from "./useAudioGraphImplCurrentDestNdRefCtxC";
+import { CAmpModulated as CAmpModulated0 } from "./useAudioGraphImplFromUseYyyNodes1";
 import { CSnareDrumFluidly1 } from "./useAudioGraphImplFComponentsSlapDrumKit1";
 
 
