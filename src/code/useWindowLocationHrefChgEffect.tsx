d1301913fc79ca19d35ecd4350cd8e3ee11bd90f
meh <eh@ah>
2022-08-23 18:24:19 +0700

    code

--- src/code/useWindowLocationHrefChgEffect.tsx
+++ src/code/useWindowLocationHrefChgEffect.tsx
@@ -11,6 +11,7 @@ import {
     intervalUsageCatchupPolicyEmitErrorOnce  ,
 
 } from "./timeoutOrIntervalPolicy";       
+import { usingInterval } from "./usingTimeoutOrInterval";
 import { useRealTimeQueryInterval1 } from "./useNonHookValue";
 
 
@@ -22,13 +23,65 @@ import { useRealTimeQueryInterval1 } from "./useNonHookValue";
 
 
 // TODO
+const useRefreshFetch = (
+   function <V>(...[f, ...intervalPolicy ] : [
+      impl: () => V ,
+      ...intervalPolicy: (
+         [(
+            Parameters<typeof usingInterval>
+         )] extends [readonly [infer F, ...(infer Args )] ]
+         ?
+         Args
+         : never
+      ) ,
+   ] ) {
+      const [v, setV] = (
+         React.useState<V >((
+            f()
+         ))
+      ) ;
+      React.useLayoutEffect(() => (
+         usingInterval(() => {
+            const value = f() ;
+            setV(() => value ) ;
+         } , ...intervalPolicy )
+      ), [] );
+      return v ;
+   }
+) ;
+const useWindowLocationHref = (
+   function () {
+      return (
+         useRefreshFetch<typeof window.location.href>(() => (
+            window.location.href
+         ) , 150 )
+      ) ;
+   }
+) ;
 const {
    useWindowLocationChgEffect ,
 } = (() => {
    return {
+      /**   
+       * this subscribes to every change of {@link window.location.href }.
+       */
       useWindowLocationChgEffect : (
-         function useWindowLocationChgEffectImpl(callback: () => void ) { 
-            // const;
+         function useWindowLocationChgEffectImpl(...[callback,  ] : [
+            callback: (
+               (e ?: { newValue: typeof window.location.href } ) 
+               => 
+               void
+            ), 
+            // deps: React.DependencyList ,
+         ]) { 
+            const value: typeof window.location.href = (
+               useWindowLocationHref()
+            ) ;
+            React.useLayoutEffect(() => (
+               callback({ newValue: value })
+               ,
+               void 0
+            ) , [value ] ) ;
          }
       ) ,
    } ;
@@ -36,3 +89,7 @@ const {
 
 
 
+
+export {
+   useWindowLocationChgEffect ,
+} ;
