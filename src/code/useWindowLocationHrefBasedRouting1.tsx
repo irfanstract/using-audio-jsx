ec9e710aed3f499df4af83e05e1dce33d502acc9
meh <eh@ah>
2022-08-25 06:30:13 +0700

    initial commit

---
+++ src/code/useWindowLocationHrefBasedRouting1.tsx
@@ -0,0 +1,89 @@
+
+
+
+
+
+// import util from "util" ;
+import { BoundedIdentityFunction, IterableOps, } from "./generalUse11";
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+} from "react";          
+import { useSeqState } from "./useSeqState";
+import { usingInterval, useIntervalDeferredValue, useAnimationFrameRefreshEffect } from "./usingTimeoutOrInterval"; 
+import { 
+    useCanForceRefresh , 
+
+    useRefreshByInterval1 ,  
+    useRefreshByInterval ,  
+ 
+} from "./usingIntervalRefresh";      
+import { useEventTarget } from "./usingTimeoutOrInterval";
+import { useDepsChgCount } from "./usingComponentMountRenderStat";      
+import { useWindowLocationHref } from "./useWindowLocationHrefChgEffect";
+
+
+
+
+
+
+;
+
+
+
+const useCtxtualRoutingState = (
+   // TODO
+   function () {
+      ;
+      const windowHref = (
+         useWindowLocationHref()
+      ) ;
+      const presentlyChoice = (
+         (
+            windowHref
+            .match(/\#(.*)$/)?.[1 ]
+            ||
+            ""
+         )
+      ) ;
+      const renderChoiceLink = (
+         function (...[{ value: whichOne } , payload ] : [
+            (
+               { value: string ; }
+            ) ,
+            React.ReactElement ,
+         ] ) {
+            return (
+               <a href={`#${whichOne }` } >
+                  { payload }
+               </a>
+            ) ;
+         }
+      ) ;
+      ;
+      return {
+         presentlyChoice , 
+         renderChoiceLink ,
+      } ;
+   }
+) ;
+
+
+
+
+export {
+   useCtxtualRoutingState ,
+} ;
