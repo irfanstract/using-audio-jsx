50e72a13e143600dc42d87088a15ecbd1c3460b8
meh <eh@ah>
2022-08-24 08:12:51 +0700

    switch to imported equivalent(s)

--- src/code/useNonHookValue.ts
+++ src/code/useNonHookValue.ts
@@ -25,6 +25,10 @@ import {
     useRealtimeQueryIntervalUsageArgsParse,
     useRealtimeQueryIntervalUsageArgsParse1,
 } from "./usingTimeoutOrIntervalScanProperties" ;
+import { 
+    useRealTimeQueryInterval1,
+    useRealTimeQueryInterval,
+} from "./usingTimeoutOrIntervalScannedValue";
 import { useDepsChgCount } from "./usingComponentMountRenderStat";      
 try {
     const em1 = { v: 3 } as const ;
@@ -46,75 +50,6 @@ const [] = [] ;
  * `deps`
 */
 const [] = [] ;
-/**  
- * both {@link useRealTimeQueryInterval } and {@link useRealTimeQueryInterval1 }  
- * are based on {@link Window.setInterval } . 
- * however,          
- * {@link useRealTimeQueryInterval } will generalise the dispatch to even outside the tick-tock(s), while 
- * {@link useRealTimeQueryInterval1} will limit the dispatch to the tick-tock(s) .
- * 
- * some critial problems :  
- * - in variation {@link useRealTimeQueryInterval } of `useRealtimeQuery` , 
- *   dispatches will not be restricted to regular intervals and instead generalised to every refresh and therefore 
- *   unexpected behaviour will arise.       
- * - in case the invocand would be performance-heavy, 
- *   {@link useRealTimeQueryInterval } will hog CPU usage.  
- * - use the closely-named {@link useRealTimeQueryInterval1 } instead .   
- * 
- * note that this does not provide `deps` ;   
- * `deps` will be unnecessary for this, due to how {@link useRefreshByInterval} works
- * 
- * @deprecated       
-*/  
-const useRealTimeQueryInterval = (       
-    function <A>(...args1 : RealTimeQueryIntervalUsageArgs<A> ): A {             
-        const {
-            props ,    
-            timeoutMillis ,           
-      
-            LE ,             
-            f ,          
-            c ,   
-            
-            forceRefresh ,            
-        } = useRealtimeQueryIntervalUsageArgsParse(...args1 ) ;                                  
-        const vl = (               
-            (function useM(): A {     
-                useDebugValue({ f });        
-                return f() ;  
-            })()               
-        ) ;        
-        return vl ;   
-    }                  
-) ;     
-/**         
- * 
- * both {@link useRealTimeQueryInterval } and {@link useRealTimeQueryInterval1 }  
- * are based on {@link Window.setInterval } . 
- * however,          
- * {@link useRealTimeQueryInterval } will generalise the dispatch to even outside the tick-tock(s), while 
- * {@link useRealTimeQueryInterval1} will limit the dispatch to the tick-tock(s)  
- * 
- * note that this does not provide `deps` ;       
- * `deps` will be unnecessary for this, due to how {@link useRefreshByInterval} works
-*/  
-const useRealTimeQueryInterval1 = (     
-    function <A>(...args1 : RealTimeQueryIntervalUsageArgs<A> ): A {             
-        const {
-            props ,    
-            timeoutMillis ,           
-      
-            LE ,             
-            f ,          
-            c ,   
-            
-            forceRefresh ,            
-        } = useRealtimeQueryIntervalUsageArgsParse(...args1 ) ;      
-        const vl = React.useMemo(() => f() , [c] ) ;
-        useDebugValue({ c, vl }) ;
-        return vl ;   
-    }                   
-) ;                      
 const {
     useNumericDigest , 
     useNumericDigestWith ,   
