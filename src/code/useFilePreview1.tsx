eab7dbdd8a8c1c17484f436ed5c9a30604b8d982
meh <eh@ah>
2022-08-26 00:06:08 +0700

    initial commit

---
+++ src/code/useFilePreview1.tsx
@@ -0,0 +1,85 @@
+
+
+// import util from "util" ;
+import { BoundedIdentityFunction, IterableOps, } from "./generalUse11";
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+} from "react";          
+import { useSeqState } from "./useSeqState";
+import { usingInterval, useIntervalDeferredValue, useAnimationFrameRefreshEffect } from "./usingTimeoutOrInterval"; 
+import { 
+    useCanForceRefresh , 
+
+    useRefreshByInterval1 ,  
+    useRefreshByInterval ,  
+ 
+} from "./usingIntervalRefresh";      
+import { useEventTarget } from "./usingTimeoutOrInterval";
+import { useDepsChgCount } from "./usingComponentMountRenderStat";      
+import { useObjectURL } from "./useEmbedSrcObjectUrl";
+
+
+
+
+
+
+
+const BasicEmbedObjectPreview = (
+   function BasicEmbedObjectPreviewC({ value } : { value: Blob ; }) {
+      const valueUrl: string = (
+         useObjectURL(value )
+      ) ;
+      const contentType: string = (
+         value.type
+      ) ;
+      const style1 = (
+         { width: "12em", height: "12em", background: "black" } as React.CSSProperties
+      ) ;
+      if (contentType.match(/^image\//) ) {
+         return (
+            <img 
+            src={valueUrl }
+            style={style1 } 
+            />
+         ) ;
+      }
+      if (contentType.match(/^(audio|video)\//) ) {
+         return (
+            <video  
+            src={valueUrl }
+            controls
+            style={style1 } 
+            />
+         ) ;
+      }
+      return (
+         <embed 
+         src={valueUrl }
+         type={contentType }
+         style={style1 } 
+         />
+      ) ;
+   }
+) ;
+
+
+
+
+
+export {
+   BasicEmbedObjectPreview ,
+} ;
