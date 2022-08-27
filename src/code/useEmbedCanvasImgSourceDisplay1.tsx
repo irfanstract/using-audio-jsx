a75df55d874fe9a78fca4d113732791dc870711c
meh <eh@ah>
2022-08-28 01:55:07 +0700

    initial commit

---
+++ src/code/useEmbedCanvasImgSourceDisplay1.tsx
@@ -0,0 +1,81 @@
+
+
+// import util from "util" ;
+import { BoundedIdentityFunction, IterableOps, } from "./generalUse11";
+import React, { 
+   // Callbacks
+   Dispatch,
+   DispatchWithoutAction, 
+
+   // Debug
+   useDebugValue,
+   // UseYyyEffect(s)
+   useLayoutEffect, useEffect, 
+   useInsertionEffect ,
+   // UseImperativeHandle
+   useImperativeHandle ,
+   // UseMemo and UseReducer and UseRefObject
+   useCallback, useMemo, useContext, useDeferredValue, 
+   useState, useReducer, 
+   useRef, 
+} from "react";          
+import { useSeqState } from "./useSeqState";
+import { usingInterval, useIntervalDeferredValue, useAnimationFrameRefreshEffect } from "./usingTimeoutOrInterval"; 
+import { 
+    useCanForceRefresh , 
+
+    useRefreshByInterval1 ,  
+    useRefreshByInterval ,  
+ 
+} from "./usingIntervalRefresh";      
+import { useEventTarget } from "./usingTimeoutOrInterval";
+import { useDepsChgCount } from "./usingComponentMountRenderStat";      
+
+
+
+
+
+
+
+// TODO
+const NativeImageSourceDisplay1 = (
+   function ({ src, validityDeps, ...props1 } : (
+      { 
+         src: (
+            CanvasImageSource 
+            & 
+            Record<"width" | "height", number > 
+         ) ; 
+         validityDeps: React.DependencyList ; 
+      }
+      &
+      JSX.IntrinsicElements["canvas"]
+   ) ): React.ReactElement {
+      const [cH, setCHandle] = (
+         useState<null | HTMLCanvasElement >(null )
+      ) ;
+      const cCH: null | CanvasRenderingContext2D = (
+         cH ?
+         cH.getContext("2d")!
+         : null
+      ) ;
+      useInsertionEffect((): void => {
+         if (cCH ) {
+            cCH.drawImage(src , 0, 0 ) ;
+         }
+      } , validityDeps ) ;
+      return (
+         <canvas 
+         ref={setCHandle }
+         {...{ width: src.width, height: src.height, ...props1 } }
+         />
+      ) ;
+   }
+) ;
+
+
+
+
+export {
+   NativeImageSourceDisplay1 ,
+} ;
