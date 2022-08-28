e06eda95dc99248287d19e2b99e28f64a600bd0c
meh <eh@ah>
2022-08-29 03:55:21 +0700

    code

--- src/code/useAudioGraphImplFComponentsCymbals.tsx
+++ src/code/useAudioGraphImplFComponentsCymbals.tsx
@@ -28,9 +28,21 @@ const CBriefHiHatFluidly = (
    () => {
       const period = 0.5 ;
       return (
-      <CAmpModulated0 value={<CFnValueByRelativeT value={({ ctxT: t }) => (t <= period ? 1 : 0 ) } /> } >
+      <CAmpModulated0 value={<CFnValueByRelativeT value={({ ctxT: t }) => ((0 <= t && t <= period) ? 1 : 0 ) } /> } >
       <CAmpModulated0 value={<CConstantValue value={2 ** -2 } /> } >
+         <CBiquadFilterModulated
+         type="highpass"
+         freqArgumentInterpretation="timedomain-normalised"
+         freqArgument={<CConstantValue value={55 / 48000 } /> }
+         >
+         <CBiquadFilterModulated
+         type="lowpass"
+         freqArgumentInterpretation="timedomain-normalised"
+         freqArgument={<CConstantValue value={10000 / 48000 } /> }
+         >
             <CWhiteNoise value={{ volume: 1 }} />
+         </CBiquadFilterModulated>
+         </CBiquadFilterModulated>
       </CAmpModulated0> 
       </CAmpModulated0>
       ) ;
