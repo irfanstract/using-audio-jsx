8b39345722b69a15661f7f54946cb4f528802e9a
meh <eh@ah>
2022-08-29 03:40:41 +0700

    imports

--- src/code/useAudioGraphImplFComponentsCymbals.tsx
+++ src/code/useAudioGraphImplFComponentsCymbals.tsx
@@ -5,7 +5,12 @@
 
           
 import { CConstantValue } from "./useAudioGraphImplFMathComponentsSupport1";
-import { CAmpModulated as CAmpModulated0 } from "./useAudioGraphImplFromUseYyyNodes1";
+import { CFnValueByRelativeT } from "./useAudioGraphImplFMathComponentsSupport11";
+import { 
+   CAmpModulated as CAmpModulated0 ,
+   CBiquadFilterModulated ,
+} from "./useAudioGraphImplFromUseYyyNodes1";
+import { CWhiteNoise } from "./useAudioGraphImplFComponentsWhiteNoise";
 import { CSnareDrumFluidly1 } from "./useAudioGraphImplFComponentsSlapDrumKit1";
 
 
