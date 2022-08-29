213d5123bdbcdf2247a3b755dbe0bb2f83bac07d
meh <eh@ah>
2022-08-29 12:59:46 +0700

    code comments

--- src/code/useEmbedSrcObjectUrl.tsx
+++ src/code/useEmbedSrcObjectUrl.tsx
@@ -37,8 +37,41 @@ import { useDepsChgCount } from "./usingComponentMountRenderStat";
 
 
 
+/**    
+ * calls {@link URL.createObjectURL } and correspondingly 
+ * schedules {@link URL.revokeObjectURL } {@link useMemo on unmount }.
+ * 
+ * due to scheduling of {@link URL.revokeObjectURL },
+ * *caching* of {@link JSX element}(s) using the (returned ) `URL` shall be avoided.
+ * 
+ * if *v* is `null`, then the return-value will be *blank URL*.
+ * this opportunity is necessary as `useYyy` calls can't be conditional.
+ * 
+ * @example
+ * {
+ *  // DO NOT cache element(s) using this URL !
+ *  const url = useObjectURL(...) ;
+ *  return (
+ *   // awful idea !
+ *   useDeferredValue((
+ *     <a target="_blank" href={url } >
+ *       VIEW-or-DOWNLOAD
+ *     </a>
+ *   ))
+ *  ) ;
+ * }
+ */
 const useObjectURL = (
-   function (...[v] : [null | Blob ] ) {
+   function (...[v] : [null | Blob ] ): string {
+   /**    
+    * {@link URL.createObjectURL } 
+    * should be accompanied with corresponding {@link URL.revokeObjectURL }.
+    * for this reason, 
+    * {@link useMemo } is not applicable as 
+    * {@link useMemo} does not provide means for *disinit*, and
+    * a combination of {@link useLayoutEffect } and {@link useState `setState(...)` } is an alternative.
+    */
+   {
       ;
       const [vUrl1, setVURl ] = (
          useState<string>("" )
@@ -58,6 +91,7 @@ const useObjectURL = (
       return (
          vUrl1
       ) ;
+   }
    }
 ) ;
 
