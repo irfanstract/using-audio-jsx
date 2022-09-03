50da4cacb3724a7583728988e8ff10eab2a5528d
meh <eh@ah>
2022-09-03 21:15:10 +0700

    switch to externally-defined equivalent

--- src/code/useAudioGraphImplFMathComponentsSupport1.tsx
+++ src/code/useAudioGraphImplFMathComponentsSupport1.tsx
@@ -4,6 +4,9 @@ import Immutable from "immutable";
 import { 
     IterableOps, PromiseReturnValue, OmitM, FrequencyAndPeriod  ,    
 } from "./generalUse11";   
+import { 
+    fromTSeqComputedValueInterpolated ,
+} from "./timeStampSeqMappedConnectedInterpolated1";
 import React, { useMemo } from "react";                
 import { 
     useRenderCount ,      
@@ -102,40 +105,6 @@ import { XDC } from "./useAudioGraphImplFComponentsSemanticsBasic";
   
      
 ;
-// TODO
-const fromTSeqComputedValueInterpolated = (
-    function (...[ts0, valueAtT ]: [
-        tStamps: readonly number[] | Immutable.Seq.Indexed<number> ,
-        valueAtT: (t: number) => number ,
-    ] ) {
-        const ts = [...ts0 ] ;
-        const tSegments = (
-            Immutable.Range(1, ts.length, 1 )
-            .toSeq()
-            .map((i: number ): [t0: number, t1: number] => [ts[i + -1 ]!, ts[i ]! ] )
-        ) ;
-        return {
-            valueAtT : (
-                (requestedT : number ) => (
-                    (
-                        tSegments
-                        .flatMap(([t0, t1 ]): ([valueAtRequestedT: number] | [] ) => {
-                            if (t0 <= requestedT && requestedT <= t1 ) {
-                                const v0 = valueAtT(t0 ) ;
-                                const v1 = valueAtT(t1 ) ;
-                                const p = (requestedT - t0 ) / (t1 - t0 ) ;
-                                return [v0 + (p * (v1 - v0 ) ) ] ;
-                            }
-                            return [] ;
-                        } ) 
-                        .first()
-                        // TODO
-                    )
-                )
-            ) ,
-        } ;
-    }
-) ;
 
       
 ;
