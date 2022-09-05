fada9cda7cd13380fc9df9034ee2cb813b1f9c30
meh <eh@ah>
2022-09-05 10:39:47 +0700

    amend

--- src/code/audioLoopDemo.tsx
+++ src/code/audioLoopDemo.tsx
@@ -218,7 +218,7 @@ export const AudioLoopDemoApp = (function () {
             ;
             return (
                 <WithSpecifiedGVisTarget value={gVisTarget} >
-                    <div key={key }>   
+                    <div>   
                     <K>
                         { instrumentsVisualiView }
                     </K>
