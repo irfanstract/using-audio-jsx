e86a91767bdc56d635bc85079b810de9f3095f97
meh <eh@ah>
2022-09-05 10:55:30 +0700

    switch to externally-defined equivalent

--- src/code/audioLoopDemo.tsx
+++ src/code/audioLoopDemo.tsx
@@ -48,6 +48,7 @@ import {
 } from "./audioLoopDemoSpecimens1";
 import { BassDrumLoopDv, HouseMusicShortBrkDemo } from "./audioLoopDemoBassDrumLoops"; 
 import { CBassLineByTimeDomainArgs } from "./audioLoopDemoSpecimens1";
+import { WithAuInstrumentsVisualisation } from "./useAudioGraphInstrumentsVisualisation1";
 import '../App.css';        
 
     
@@ -350,68 +351,3 @@ export const AudioLoopDemoApp = (function () {
         }  
     ) ;
 })() ;           
-const WithAuInstrumentsVisualisation = (
-        function ({ children: c1 } : (
-            { children: object & React.ReactElement ; }
-        ) ) {
-            ;
-            const tT = 0 ;
-            const [gVisTarget, setGVisTarget] = (
-                React.useState<null | SVGElement >(null )
-            ) ;
-            ;
-            const instrumentsVisualiView = (
-                <div 
-                title={(
-                    `Graphical visualisation of the simulated instruments in time-lapse.`
-                )} 
-                >
-                <svg 
-                viewBox="0 0 512 16" 
-                preserveAspectRatio={"none" } 
-                style={{ height: "3em", width: "98%", background: "yellow" }}
-                >
-                    <WithCurrentDestNdRef>
-                        { ({ currentTime: aCtxT }) => (
-                            <g 
-                            /**   
-                             * string-concatenation on SVG JSX
-                             * should be substituted with combination between CSS *custom properties* and *transforms*
-                             * to maintain performance `XD`
-                             */
-                            style={{
-                                transform: (
-                                    [
-                                        `scale(24, 16 )` ,
-                                        `translate(calc(var(--COMPONENTLOCALLY-DATA-ACTX-T ) * -1px ) , 0px )` ,
-                                    ].join(" ")
-                                ) ,
-                                ...({
-                                    "--COMPONENTLOCALLY-DATA-ACTX-T" : (
-                                        (typeof aCtxT === "number") ? aCtxT : tT 
-                                    ) ,
-                                }) ,
-                            }}
-                            >
-                                <g ref={setGVisTarget } />
-                            </g>
-                        ) }
-                    </WithCurrentDestNdRef>
-                </svg>
-                </div>
-            ) ;
-            ;
-            return (
-                <WithSpecifiedGVisTarget value={gVisTarget} >
-                    <div>   
-                    <K>
-                        { instrumentsVisualiView }
-                    </K>
-                    <K>
-                        { true && c1 }    
-                    </K>
-                    </div>                 
-                </WithSpecifiedGVisTarget>  
-            ) ;
-        }
-) ;
