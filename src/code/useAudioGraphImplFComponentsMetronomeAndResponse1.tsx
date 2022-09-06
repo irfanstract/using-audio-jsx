40f8602c8f02fcbd4b304b59ee964d0f0c40dfe8
meh <eh@ah>
2022-09-06 10:02:14 +0700

    new

---
+++ src/code/useAudioGraphImplFComponentsMetronomeAndResponse1.tsx
@@ -0,0 +1,73 @@
+
+import { 
+   Iterable,
+   IterableOps , 
+    
+} from "./generalUse11";     
+import { 
+   AudioTrackConcatClippingMode , 
+   avTrackConcatShallPropagate, 
+   OmitOrPropagate,          
+
+} from "./timelineConcatClippingMode";    
+import React, { useReducer, useState } from "react";  
+import { ComponentProps } from "./commonElementsTypes";
+import { K } from "./commonElements";         
+import { CBC } from "./useStateInCallback";  
+import { useRealTimeQueryInterval1 } from "./useNonHookValue";   
+
+          
+
+// domain-imports        
+import * as tCtxs from "./useAudioGraphImplAbsoluteTCtx1";        
+import {
+   WithDelay ,              
+   LoopingWithPeriod as LoopingWithPeriodSimple ,  
+   LwpPayloadCallback , 
+    
+   WithCurrentTInfo as WithCurrentScheduledTInfo ,              
+                 
+   //  
+} from "./useAudioGraphImplAbsoluteTCtx1";      
+import { CFnValueByRelativeT } from "./useAudioGraphImplFMathComponentsSupport11";
+import {
+   Consm as WithCurrentDestNdRef , 
+   useWithCurrentSideTapPtRef , 
+   useCurrentDestNdRefAndCtxT ,
+
+} from "./useAudioGraphImplCurrentDestNdRefCtx";           
+import { 
+   PreFTAndPostFTProps ,
+   WithAutoUnmount , 
+   withAutoUnmount , 
+   passageStateBy1 ,   
+
+} from "./useAudioGraphImplFComponentsSlapCompAutoUnmounting";   
+import { 
+   MetronomeAndResponseGraph ,
+   MetronomeGraphingCallback ,
+   
+} from "./useAudioGraphImplFComponentsLoopingWrapper1";
+
+// CSS imports
+         
+  
+
+// destructuring  
+const {         
+} = tCtxs ;         
+ 
+
+
+
+
+
+
+  
+   
+                       
+export {
+   LwpPayloadCallback ,
+   MetronomeAndResponseGraph ,
+   MetronomeGraphingCallback ,
+} ;
