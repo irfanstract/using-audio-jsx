8eb77a6b5dcceea5a886cb9b20f63dba4066fb6d
meh <eh@ah>
2022-09-06 22:54:09 +0700

    reindentation

--- src/code/audioLoopDemoTimingCtxComponents.tsx
+++ src/code/audioLoopDemoTimingCtxComponents.tsx
@@ -42,111 +42,111 @@ const {
        
 ;
 const TPTC = (() => {
-    ;
-    /**    
-     * {@link tCtxs.currentTCtx.Provider }
-     */
-    const {
-        Provider: TP ,     
-    } = (
-        tCtxs.currentTCtx             
-    ) ;                        
-    const {  
-        Consumer: TCP ,    
-    } = tCtxs.currentTInfCtx ;
-    const withADestCtxTCompletionStatCtx = (() => {
-        type InnerRenderer = (
-            (ctx: {
-                absolT0 : number ;
-                absolT1 : number ;
-                tScale : number ;
-                relativeTDiff : number ;
-            } )
-            => React.ReactElement
-        ) ;
-        type OuterRenderer = (
-            (ctx : {
-                withADestCtxCompletionStat : (
-                    (payload : InnerRenderer ) => React.ReactElement
-                ) ;
-            } )
-            => React.ReactElement
-        ) ;
-        return (
-            (...[payload0 ] : [
-                payload : (
-                    OuterRenderer
-                ) ,
-            ] ): React.ReactElement => {
-                return (
-                    <TCP>
-                    { ({ 
-                        t: absolT0, 
-                        /**   
-                         * cannot use `tScale` from this `ctx`, as
-                         * in general the `tScale` at the *site* of the *inner payload* can be different.
-                         */
-                    }) => {
-                        ;
-                        const withADestCtxCompletionStat = (
-                            (...[payload1 ] : (
-                                [InnerRenderer ]
-                            )) => (
-                                <TCP>
-                                { ({ 
-                                    /**   
-                                     * again,
-                                     * cannot use `tScale` from the outer `ctx`, as
-                                     * in general the `tScale` here can be different.
-                                     */
-                                    tScale: tScale, 
-                                }) => (
-                                <WithCurrentDestNdRef>  
-                                { ({ currentTime: absolT1 }) => (
-                                    (typeof absolT1 === "number") ?
-                                    ((): React.ReactElement => {
-                                        const relativeTDiff = (
-                                            (absolT1 - absolT0 ) / tScale
-                                        ) ;
-                                        return (
-                                            payload1({ 
-                                                absolT0: absolT0, 
-                                                absolT1: absolT1, 
-                                                tScale: tScale,
-                                                relativeTDiff ,
-                                            })
-                                        ) ;
-                                    })()
-                                    : <></>
-                                ) }
-                                </WithCurrentDestNdRef>
-                                ) }
-                                </TCP>
-                            )
-                        ) ;
-                        return (
-                            payload0({ withADestCtxCompletionStat })
-                        ) ;
-                    } }
-                    </TCP>
-                ) ;
-            }
-        ) ;
-    })() ;
-    const WithADestCtxTCompletionStatCtx = (
-        ({ children: payload } : (
-            { children: Parameters<typeof withADestCtxTCompletionStatCtx >[0 ] ; }
-        ) ) => (
-            withADestCtxTCompletionStatCtx(payload )
-        )
-    ) ;
-    ;
-    return {
-        TCP ,
-        TP ,
-        withADestCtxTCompletionStatCtx ,
-        WithADestCtxTCompletionStatCtx ,
-    } ;
+   ;
+   /**    
+    * {@link tCtxs.currentTCtx.Provider }
+    */
+   const {
+      Provider: TP ,     
+   } = (
+      tCtxs.currentTCtx             
+   ) ;                        
+   const {  
+      Consumer: TCP ,    
+   } = tCtxs.currentTInfCtx ;
+   const withADestCtxTCompletionStatCtx = (() => {
+      type InnerRenderer = (
+         (ctx: {
+               absolT0 : number ;
+               absolT1 : number ;
+               tScale : number ;
+               relativeTDiff : number ;
+         } )
+         => React.ReactElement
+      ) ;
+      type OuterRenderer = (
+         (ctx : {
+               withADestCtxCompletionStat : (
+                  (payload : InnerRenderer ) => React.ReactElement
+               ) ;
+         } )
+         => React.ReactElement
+      ) ;
+      return (
+         (...[payload0 ] : [
+               payload : (
+                  OuterRenderer
+               ) ,
+         ] ): React.ReactElement => {
+               return (
+                  <TCP>
+                  { ({ 
+                     t: absolT0, 
+                     /**   
+                      * cannot use `tScale` from this `ctx`, as
+                      * in general the `tScale` at the *site* of the *inner payload* can be different.
+                      */
+                  }) => {
+                     ;
+                     const withADestCtxCompletionStat = (
+                           (...[payload1 ] : (
+                              [InnerRenderer ]
+                           )) => (
+                              <TCP>
+                              { ({ 
+                                 /**   
+                                  * again,
+                                  * cannot use `tScale` from the outer `ctx`, as
+                                  * in general the `tScale` here can be different.
+                                  */
+                                 tScale: tScale, 
+                              }) => (
+                              <WithCurrentDestNdRef>  
+                              { ({ currentTime: absolT1 }) => (
+                                 (typeof absolT1 === "number") ?
+                                 ((): React.ReactElement => {
+                                       const relativeTDiff = (
+                                          (absolT1 - absolT0 ) / tScale
+                                       ) ;
+                                       return (
+                                          payload1({ 
+                                             absolT0: absolT0, 
+                                             absolT1: absolT1, 
+                                             tScale: tScale,
+                                             relativeTDiff ,
+                                          })
+                                       ) ;
+                                 })()
+                                 : <></>
+                              ) }
+                              </WithCurrentDestNdRef>
+                              ) }
+                              </TCP>
+                           )
+                     ) ;
+                     return (
+                           payload0({ withADestCtxCompletionStat })
+                     ) ;
+                  } }
+                  </TCP>
+               ) ;
+         }
+      ) ;
+   })() ;
+   const WithADestCtxTCompletionStatCtx = (
+      ({ children: payload } : (
+         { children: Parameters<typeof withADestCtxTCompletionStatCtx >[0 ] ; }
+      ) ) => (
+         withADestCtxTCompletionStatCtx(payload )
+      )
+   ) ;
+   ;
+   return {
+      TCP ,
+      TP ,
+      withADestCtxTCompletionStatCtx ,
+      WithADestCtxTCompletionStatCtx ,
+   } ;
 })() ;
 
 
