a6777a501c3bf984abc7c2350bb8576368af4ac5
meh <eh@ah>
2022-09-11 23:30:07 +0700

    add

--- src/code/generalUseTypeDefs.ts
+++ src/code/generalUseTypeDefs.ts
@@ -49,6 +49,35 @@ type EnumValue<A extends {} > = (
 type SameParametersAndReturnType<A extends (...args: any ) => any > = (             
     { (...a: Parameters<A> ): ReturnType<A> ; }
 ) ;
+// type ParametersCompacted<A extends readonly unknown[] > = (
+//    never
+//    | (
+//       [A] extends [readonly [] ] ? 
+//       [] : never
+//    )
+//    | (
+//       // '[{}] extends A ?' is not enough because 
+//       // '[{}] extends readonly [NonNullable<unknown> ]'.
+//       [[{}], A ] extends [A, readonly [{} ? ] ] ? 
+//       [{}? ] : never
+//    )
+//    | (
+//       [A] extends [infer Arg1, ...(infer Args2) ] ?
+//       [Arg1, ...ParametersCompacted<Args2 > ]
+//       : never
+//    )
+// ) ;
+/**   
+ * this *type-alias* 
+ * takes care 
+ * deciding whether the parameter can be made *optional*
+ * (which will only be the case when(ever) it has *zero* *required property* ).
+ */
+ type OptionsCouldBeOmittedAltogether<A extends {} > = (
+    [A, [{}, ] ] extends [{}, readonly [A, ] ] ? 
+    [A? ]
+    : [A ]
+ );
 
             
 
@@ -96,5 +125,6 @@ export type {
     EnumValue ,      
 
     SameParametersAndReturnType , 
+    OptionsCouldBeOmittedAltogether ,
 } ;
              
