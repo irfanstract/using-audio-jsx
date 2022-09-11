c13769a2ca91914cf01e5a6e32240594981a1008
meh <eh@ah>
2022-09-11 18:43:57 +0700

    add

--- src/code/useMediaStreamAsBlobSeq1.tsx
+++ src/code/useMediaStreamAsBlobSeq1.tsx
@@ -42,6 +42,25 @@ import { useMediaStreamRec } from "./useMediaStreamRecordingSwitchToDest1";
 
 /**    
  * this `useYyy`
+ * 1) is meant for use with `InterlaceBlobSeqMediaPlayer`
+ * 1) will return 
+ *    either `null` or 
+ *    a {@link Blob } playing-back the last-three-second from/of it 
+ * 2) will cause 
+ *    {@link React.useLayoutEffect *periodic refresh* } 
+ *    accompanied by *update* in the return-value
+ * 
+ */
+const useMediaStreamAsInterlaceAsBlob : (
+   (src: null | MediaStream ) 
+   => (null | Blob )
+) = (
+   (...args ) => (
+      useMediaStrmPollNextAsBlob(...args )
+   )
+) ;
+/**    
+ * this `useYyy`
  * 1) will return 
  *    either `null` or 
  *    a {@link Blob } playing-back the last-three-second from/of it 
@@ -82,5 +101,6 @@ const useMediaStrmPollNextAsBlob = (
 
 
 export {
+   useMediaStreamAsInterlaceAsBlob ,
    useMediaStrmPollNextAsBlob ,
 } ;
