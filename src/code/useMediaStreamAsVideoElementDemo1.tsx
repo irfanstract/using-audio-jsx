b1a27e76c91b30ec3932ad460bd6efff96a659cb
meh <eh@ah>
2022-09-16 20:38:13 +0700

    add

---
+++ src/code/useMediaStreamAsVideoElementDemo1.tsx
@@ -0,0 +1,46 @@
+
+import { IterableOps } from "./generalUse11";
+import { linearSlidingStateAtT } from "./timeDomainClosedGradientSampling1";
+import { SupportedFShiftAmtInterpretation } from "./audioFreqShiftInterpretation1";
+import { periodicTRestrictedLinearlyApproachedValueAtT1 } from "./audioTDomainPeriodicPlotSamplingAtT1";
+import React, { useReducer, useState } from "react";   
+import { K, ComponentProps, ContextReturnType } from "./commonElements";      
+import { CBC } from "./useStateInCallback";
+import { 
+    useOneWayCompletiveState ,      
+            
+} from "./useCompletion";    
+import { useAsyncMemo } from "./useAsyncMemo";          
+
+     
+   
+// domain-imports         
+import { useMediaReadStreamFromOfStillCssFill, } from "./useMediaStreamFromOfNativeCanvas1";
+import { useMediaReadStreamDemoBackForthColorAnim, } from "./useMediaStreamGenBackForthColorAnim1";
+import { MediaStreamVideoC, } from "./useMediaStreamAsVideoElement1";
+
+    
+
+
+
+
+
+
+export default (
+   function MediaStreamAsVideoElementDemo() {
+      const videoS: MediaStream = (
+         useMediaReadStreamDemoBackForthColorAnim({
+            period: 8,
+            outputRefreshRate: 10,
+         })
+      ) ;
+      return (
+         <div>
+         <MediaStreamVideoC 
+         src={videoS }
+         autoPlay
+         />
+         </div>
+      ) ; // TODO
+   }
+) ;
