3118ca89eb963c63d26946376cb5d2e44f36b308
meh <eh@ah>
2022-09-23 13:25:43 +0700

    add into docs

--- src/code/generalUseTypeDefs.ts
+++ src/code/generalUseTypeDefs.ts
@@ -55,6 +55,10 @@ type SameParametersAndReturnType<A extends (...args: any ) => any > = (
  * deciding whether the parameter can be made *optional*
  * (which will only be the case when(ever) it has *zero* *required property* ).
  * 
+ * no need to care about updating the `?:` on every change in `props`, as
+ * using this idiom the change will happen automatically as needed.
+ * see below.
+ * 
  * @example
  * 
  * // all props are optional here, but ... 
