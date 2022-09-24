bc7d644572f12385f112fa61db078c2d3f11ccaf
meh <eh@ah>
2022-09-25 00:56:33 +0700

    add

---
+++ src/code/usingJsonStringifyingElement1.tsx
@@ -0,0 +1,58 @@
+
+import { 
+   BoundedIdentity, 
+   BoundedIdentityFunction, 
+   OverloadedParameters ,
+   OverloadedReturns ,
+   Unix ,     
+   IterableOps , 
+   Immutable ,    
+   Seq1 ,
+} from "./generalUse11";       
+import React, { 
+   useState, useReducer, useLayoutEffect, useEffect, useCallback, useMemo, useContext, useDeferredValue ,
+} from "react";
+
+
+
+
+
+
+
+
+
+const DictDisplayElement : (
+   React.FC<(
+      OverloadedParameters<(typeof JSON)["stringify"]> extends [infer Payload, ...(infer Args) ] ?
+      { 
+         value : (
+            null | ((object | string | number ) )
+         ) ; 
+         fmt ?: (
+            [((Args[0] & ((...args : any[] ) => any ) )) ? , Args[1] ? ]
+         ) ;
+      }
+      : never
+   )>
+) = (
+   (props ) => {
+      const { value, fmt = [undefined, 2, ], } = props ;
+      const valuFmatted = (
+         React.useMemo(() => (
+            JSON.stringify(value, ...fmt, )
+         ) , [value, fmt, ] )
+      ) ;
+      return (
+         <code className="long-enough">
+            { valuFmatted }
+         </code>
+      ) ;
+   }
+) ;
+
+
+
+
+export {
+   DictDisplayElement ,
+} ;
