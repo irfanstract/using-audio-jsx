cef99b8e7c18c9e4ad11ae04c7aa23659f18d692
meh <eh@ah>
2022-09-24 15:28:43 +0700

    add

--- src/code/useAudioGraphImplBasicLoopingComponentProps1.tsx
+++ src/code/useAudioGraphImplBasicLoopingComponentProps1.tsx
@@ -29,8 +29,61 @@ import { K, ComponentProps, ContextReturnType, arrayIndexedOrderedList } from ".
 
 
 
-;
+type LoopingComponentPropsWithPeriodB = (
+   /**  logical properties   */   
+   {             
+
+      /**       
+         * the payload will be the *repeand*.        
+         */  
+      children : (   
+            React.ReactNode  
+            | 
+            LwpPayloadCallback
+      ) ;
+
+      /**    
+         * this is (supposed) to define the domain properties (excluding `children`).
+         */
+      value: {      
+            period: number ;                                                
+            initialOffset ?: number ;         
+            // clippingMode ?: AudioTrackConcatClippingMode ;          
+      } ;                     
+
+   }    
+) ;
+const LoopingComponentPropsWithPeriodB = {} ; // TS-1205
+/**           
+ * possible return values of implemts : 
+ * - {@link LwpPayloadCallback.OmittedItem } - omitted item
+ * - {@link LwpPayloadCallback.EmptyItem } - item as blank 
+ * - concrete elements    
+ */
+type LwpPayloadCallback = (        
+   (ctx: {
+      /**    
+         * relative to the whole component. 
+         * */ 
+      perInstanceRelativeT: number ;  
+
+      /**     
+         * the component-level {@link useCurrentTInf `absoluteTValue` }.  
+      */
+      componentLevelAbsoluteT : number ; 
+   } )  
+   => 
+   (LwpPayloadCallback.OmittedItem | LwpPayloadCallback.EmptyItem | React.ReactElement )  
+) ;      
+namespace LwpPayloadCallback {
+   export type EmptyItem = false ;   
+   export type OmittedItem = null ;        
+   const bar = {} ; // TS-1205            
+} ;        
 
 
 
-export {} ;
+export {
+   LwpPayloadCallback ,
+   LoopingComponentPropsWithPeriodB as LoopingComponentPropsWithPeriod ,
+} ;
