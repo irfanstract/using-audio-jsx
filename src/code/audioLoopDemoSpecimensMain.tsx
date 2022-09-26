fff56b8dffcb39f08743731e106c34604ed386fd
meh <eh@ah>
2022-09-26 11:04:07 +0700

    new

---
+++ src/code/audioLoopDemoSpecimensMain.tsx
@@ -0,0 +1,87 @@
+
+import { IterableOps } from "./generalUse11";
+import { Iterable } from "./generalUse11";
+import { linearSlidingStateAtT } from "./timeDomainClosedGradientSampling1";
+import { SupportedFShiftAmtInterpretation } from "./audioFreqShiftInterpretation1";
+import { periodicTRestrictedLinearlyApproachedValueAtT1 } from "./audioTDomainPeriodicPlotSamplingAtT1";
+import React, { useReducer, useState } from "react";   
+import { K, ComponentProps, ContextReturnType } from "./commonElements";      
+import { CBC } from "./useStateInCallback";
+import { 
+    useOneWayCompletiveState ,      
+            
+} from "./useCompletion";    
+import { useAsyncMemo } from "./useAsyncMemo";          
+import { useRealTimeQueryInterval1, } from "./usingTimeoutOrIntervalScannedValue";
+
+     
+   
+// domain-imports         
+import * as tCtxs from "./useAudioGraphImplAbsoluteTCtx1";   
+import TPTC from "./audioLoopDemoTimingCtxComponents";
+import { 
+    CHalfSecndBeepAtAbsoluteT ,     
+} from "./useAudioGraphImplFromUseYyyNodes1";  
+import {            
+    CurrentCtxTInfoDisplay ,    
+    Prv1 ,                
+    useADestNdRefCtx1 , 
+    Consm as WithCurrentDestNdRef ,
+    
+} from "./useAudioGraphImplCurrentDestNdRefCtx";     
+import { WGD_DIV } from "./useAudioGraphImplCurrentDestNdRefCtx";             
+import { 
+    CFnValue1 ,
+    
+    CHalfSecndBeep1 , CPersistingBeep , CWaveTable1 , CWhiteNoise ,    
+    CBassDrumKickFluidly1 ,
+    CSnareDrumFluidly1 , 
+    
+    CAmpModulated , CBiquadFilterModulated ,  CFreqDmAnalyF ,  CConstantValue , CAmpModulated0 ,    
+
+    CmInHarmonics ,
+    MetronomeAndResponseGraph , 
+    
+} from "./audioLoopDemoComponents1"; 
+import { CBriefHiHatFluidly } from "./useAudioGraphImplFComponentsCymbals";
+import * as BBVR from "./audioLoopDemoSpecimens";  
+import {
+    CBassDrumLoop , 
+    CAmbientNoise , 
+    WithNSecondsFadeInBF ,     
+
+} from "./audioLoopDemoSpecimens1";
+import { BassDrumLoopDv, HouseMusicShortBrkDemo } from "./audioLoopDemoBassDrumLoops"; 
+import { CSnareDrumJammer, } from "./useAudioGraphForSnareDrumJammer1";
+import { CBassLineByTimeDomainArgs } from "./audioLoopDemoSpecimens1";
+
+    
+
+const {
+    currentTCtx , 
+    currentTScaleCtx ,         
+    currentTInfCtx ,    
+
+    CurrentTDisplay , 
+    WithDelay ,     
+    WithSlowdown,
+    LoopingWithPeriod , 
+
+    AsGVis ,
+    WithSpecifiedGVisTarget ,
+} = tCtxs ;
+const { //
+    TCP ,
+    TP ,
+    withADestCtxTCompletionStatCtx ,
+    WithADestCtxTCompletionStatCtx ,
+} = TPTC ;
+
+
+
+
+
+
+
+
+
