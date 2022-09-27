b710c3d48155d1ea224ad36cb6f93aaa34e4a65f
meh <eh@ah>
2022-09-27 09:43:40 +0700

    add

--- src/code/usingJsonStringifyingElement1.tsx
+++ src/code/usingJsonStringifyingElement1.tsx
@@ -57,6 +57,122 @@ const DictDisplayElementSimple = (
       ) ;
    }
 ) ;
+const CKeyValueTableRowElement : (
+   React.FC<{ 
+      children : { 
+         key: string ; 
+         valueRender: (
+            (ctx: { assignedId: string ; } ) 
+            => React.ReactElement
+         ) ; 
+      } ;
+   }>
+) = (
+   ({ children: { key, valueRender, }, }) => {
+      const idx = (
+           Array.from(React.useId() )
+           .map(v => v.replace(/\W/g, () => "-" ) )
+           .join("")
+      ) ;
+      return (
+         <tr key={key } title={`Property '${key }'.` } >
+            <td>
+               <label htmlFor={idx } style={{ display: "block" }}>
+                  <code>{ (typeof key === "string") ? `'${ key }'` : "" }</code>
+               </label>
+            </td>
+            <td>
+               { valueRender({ assignedId: idx, }) }
+            </td>
+         </tr>
+      ) ;
+   }
+) ;
+//
+const DictDisplayElementPro = (
+   ({ value, ...props }: (
+      ComponentProps<typeof DictDisplayElementSimple >
+   ) ) => {
+      const renderWithSameSettings = (
+         (value: unknown ): React.ReactElement => (
+            (
+               typeof value === "object" || typeof value === "number" || typeof value === "string" ?
+               <DictDisplayElementPro 
+               value={value } 
+               {...{ ...props } } 
+               />
+               : null
+            )
+            || (
+               <>{ value }</>
+            )
+         )
+      ) ;
+      {
+         if (value !== null && (typeof value === "object") ) {
+            if (Object.getPrototypeOf(value ) === Object.prototype ) {
+               const kvs = Object.entries(value ) ;
+               return (
+                  <JsonSpecificTBodyElement>
+                     { (
+                        kvs
+                        .map(([key, value, ], i ) => (
+                           <K key={key }>
+                           <CKeyValueTableRowElement>
+                           { { 
+                              key, 
+                              valueRender: ({ assignedId: id, }) => (
+                                 <div id={id } style={{ display: "flex", flexDirection: "column", }} >
+                                    { renderWithSameSettings(value ) }
+                                 </div>
+                              ) , 
+                           } }
+                           </CKeyValueTableRowElement>
+                           </K>
+                        ) )
+                     ) }
+                  </JsonSpecificTBodyElement>
+               ) ;
+            }
+            if (Array.isArray(value ) ) {
+               const children : unknown[] = (
+                  value
+               ) ;
+               const childrenRendered: React.ReactElement[] = (
+                  // TODO
+                  children
+                  .map((e: unknown ): React.ReactElement => (
+                     renderWithSameSettings(e )
+                  ))
+               ) ;
+               return (
+                  <JsonSpecificListElement>
+                     { (
+                        childrenRendered
+                        .map((e, i) => (
+                           /**   
+                            * depending on domain, using `i` as `key` might therefore be inappropriate ;
+                            * we leave this responsibility to callers
+                            * 
+                            */
+                           <li key={i} >
+                              { e }
+                           </li>
+                        ) )
+                     ) }
+                  </JsonSpecificListElement>
+               ) ;
+            }
+         }
+      }
+      return (
+         <DictDisplayElementSimple 
+         value={value } 
+         {...props } 
+         />
+      ) ;
+   }
+) ;
 const DictDisplayElement : (
    React.FC<(
       ComponentProps<typeof DictDisplayElementSimple >
